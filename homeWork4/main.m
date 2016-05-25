2f9872e82bcd70bbc341194a9aec87b75a0c75bb
Sergejs Lastovskis <Sergejs.lastovskis@gmail.com>
2016-05-25 20:11:36 +0300

    Initial Commit

---
+++ homeWork4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  homeWork4
+//
+//  Created by Sergejs Lastovskis on 25/05/16.
+//  Copyright © 2016 Sergejs Lastovskis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
